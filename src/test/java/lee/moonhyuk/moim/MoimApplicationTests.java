fc0c2fee902013d6f9ea714e21c132ea6b90f484
MoonHKLee <mf311110@gmail.com>
2023-06-04 22:52:24 +0900

    Initial commit

---
+++ src/test/java/lee/moonhyuk/moim/MoimApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.moonhyuk.moim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
